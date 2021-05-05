da0d0b4635e17beb1335b549968a83ce3f508a06
RomanP92 <roman.na.gogle@gmail.com>
2021-05-06 01:30:34 +0300

    Changed the core/pages/home template (if tag) to check the logout Redefined the login form, to match the html template styles

--- core/views/login.py
+++ core/views/login.py
@@ -1,27 +1,29 @@
-# from django.contrib import messages
+from django.contrib import messages
 from django.contrib.auth.views import LoginView
-# from django.shortcuts import resolve_url
-#
-# from core.models import UserProfile
-# from django.conf import settings
-#
-#
+from django.shortcuts import resolve_url
+# from django.urls import reverse_lazy
+
+from core.models import UserProfile
+from django.conf import settings
+from core.forms.login import AuthenticationUserForm
 
 
 class CustomLoginView(LoginView):
     template_name = 'core/pages/login.html'
+    authentication_form = AuthenticationUserForm
+    # next =
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['version'] = settings.VERSION
-#         return context
-#
-#     def get_success_url(self):
-#         # TODO: need fix redirect by 'next' GET param (see django.contrib.auth.views.LoginView.get_redirect_url())
-#         user_profile = UserProfile.objects.filter(user=self.request.user).first()
-#         if user_profile is not None:
-#             view_name = "farmer:main_page" if user_profile.is_supplier else "buyer:home"
-#         else:
-#             messages.error(self.request, 'Профиль не найден. Обратитесь к администратору.')
-#             view_name = "login"
-#         return resolve_url(view_name)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['version'] = settings.VERSION
+        return context
+
+    # def get_success_url(self):
+    #     # TODO: need fix redirect by 'next' GET param (see django.contrib.auth.views.LoginView.get_redirect_url())
+    #     user_profile = UserProfile.objects.filter(user=self.request.user).first()
+    #     if user_profile is not None:
+    #         view_name = "farmer:main_page" if user_profile.is_supplier else "buyer:home"
+    #     else:
+    #         messages.error(self.request, 'Профиль не найден. Обратитесь к администратору.')
+    #         view_name = "login"
+    #     return resolve_url(view_name)
