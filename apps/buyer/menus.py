1ebb598b10739a7d47ba0d3bfa9b4deda57c1d72
Egor N. Kostyuk <egor@kostyuk.by>
2021-05-24 00:16:06 +0300

    buyer: buyer_left_sidebar menu implementation

---
+++ apps/buyer/menus.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+from menu import Menu, MenuItem
+
+# myProducts.html
+Menu.add_item("buyer_left_sidebar", MenuItem("Витрина",
+                                             reverse("buyer:home"),
+                                             weight=10,
+                                             icon="fas fa-store"))
+# index.html
+Menu.add_item("buyer_left_sidebar", MenuItem("Корзина",
+                                             reverse("buyer:cart"),
+                                             weight=20,
+                                             icon="fas fa-shopping-basket"))
+# payments.html
+Menu.add_item("buyer_left_sidebar", MenuItem("Платежи",
+                                             reverse("buyer:home"),
+                                             weight=20,
+                                             icon="fas fa-money-bill-wave-alt"))
+# rating.html
+Menu.add_item("buyer_left_sidebar", MenuItem("Рейтинг фермеров",
+                                             reverse("buyer:home"),
+                                             weight=20,
+                                             icon="fas fa-star"))
