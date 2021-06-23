d3becc3746c9efe956ab7988861747e13be1d71b
Stanislau Klimovich <stasklimovich299@gmail.com>
2021-06-23 09:57:32 +0300

    buyer: empty cart appearance implemented

--- apps/buyer/views/cart.py
+++ apps/buyer/views/cart.py
@@ -3,3 +3,7 @@ from .base import BuyerBasePagesView
 
 class BuyerCartView(BuyerBasePagesView):
     template_name = 'buyer/pages/buyer-cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
