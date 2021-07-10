6e50fcc90060484effa042673dca534e6760759d
Egor N. Kostyuk <egor@kostyuk.by>
2021-07-10 18:39:49 +0300

    core, farmer: left menu was reworked, jquery loading fix

--- apps/farmer/views/base.py
+++ apps/farmer/views/base.py
@@ -1,13 +1,24 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from django.views.generic import TemplateView
 
+from core.menu_data import MenuItem
+from core.views.base import BaseView
 from ..forms.profile import FarmerAreaProfileForm
 
 
-class FarmerBasePagesView(LoginRequiredMixin, TemplateView):
+class FarmerBasePagesView(BaseView, LoginRequiredMixin):
     login_url = '/login/'
+
+    # This property is using by BaseView for menu generation
+    @property
+    def left_sidebar_menu_items(self):
+        return [
+            MenuItem(title="Мои продукты", link=reverse("farmer:foodstuffs"), icon="fab fa-pagelines"),
+            MenuItem(title="Заказы", link=reverse("farmer:orders"), icon="fas fa-shopping-basket"),
+            MenuItem(title="Платежи", link=reverse("farmer:foodstuffs"), icon="fas fa-money-bill-wave-alt"),
+            MenuItem(title="Мой рейтинг", link=reverse("farmer:foodstuffs"), icon="fas fa-star"),
+        ]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,7 +26,6 @@ class FarmerBasePagesView(LoginRequiredMixin, TemplateView):
         if hasattr(self.request.user, 'profile'):
             context['farmer_profile_api_url'] = reverse('farmer-api:profile',
                                                         kwargs={'pk': self.request.user.profile.id})
-
             context['farmer_profile_form'] = FarmerAreaProfileForm(instance=self.request.user.profile)
         else:
             messages.error(self.request, 'Farmer profile can not found.')
---
+++ core/views/base.py
@@ -0,0 +1,32 @@
+import logging
+from abc import ABC, abstractmethod
+
+from django.views.generic import TemplateView
+
+class BaseView(ABC, TemplateView):
+    """
+    Need to implement in child class property
+    called "left_sidebar_menu_items" which get list with menu items like below:
+
+    @property
+    def left_sidebar_menu_items(self):
+        return [
+            MenuItem(title="Мои продукты", link=reverse("farmer:foodstuffs"), icon="fab fa-pagelines"),
+            MenuItem(title="Заказы", link=reverse("farmer:orders"), icon="fas fa-shopping-basket"),
+        ]
+
+    """
+
+    @property
+    @abstractmethod
+    def left_sidebar_menu_items(self):
+        pass
+
+    def get_selected_left_menu_item(self, request):
+        return request.path
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["left_sidebar_menu_items"] = self.left_sidebar_menu_items
+        context["selected_left_menu_item"] = self.get_selected_left_menu_item(self.request)
+        return context
