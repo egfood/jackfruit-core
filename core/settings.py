8ff625625d9f4ab7b50279d8f68452a53b5280c4
Egor N. Kostyuk <egor@kostyuk.by>
2022-07-03 23:17:18 +0300

    core: make TIME_ZONE as env setting fixed #40

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'ru-RU'
-TIME_ZONE = 'Europe/Minsk'
+TIME_ZONE = env.str("TIME_ZONE", "Europe/Minsk")
 
 USE_I18N = True
 USE_L10N = True
